8e3f0faeac0ef6859da9d266d04450f0f340e2b9
Solonarv <nstamm@gmx.de>
2016-03-02 18:48:15 +0100

    Simplified ConstraintWitness.Plugin to minimally expose the compile error

--- plugin/ConstraintWitness/Plugin.hs
+++ plugin/ConstraintWitness/Plugin.hs
@@ -1,13 +1,13 @@
 module ConstraintWitness.Plugin where
 
-import ConstraintWitness.Plugin.Witness (witnessPlugin)
+-- import ConstraintWitness.Plugin.Witness (witnessPlugin)
 -- import ConstraintWitness.TcPlugin.Instances
 
 import GhcPlugins (Plugin(..), defaultPlugin)
 
 -- The actual plugin we're exporting
 plugin :: Plugin
-plugin = defaultPlugin {
+plugin = defaultPlugin {-
     tcPlugin = const $ Just $ witnessPlugin
-    }
+    -}
 
